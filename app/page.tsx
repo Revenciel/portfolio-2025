8b531cb1105fc5a445a7c64586affa9e54c19bef
Revenciel <clarkybox@gmail.com>
2025-07-31 08:27:15 -0400

    added scss, styling work

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import getPostMetadata from "@/utils/getPostMetadata";
 //import Image from "next/image";
 import Hero from "@/components/Hero/Hero";
 import ProjectCard from "@/components/ProjectCard/ProjectCard";
-// import styles from "./page.module.css";
-// use styles.classname if using the css module
+
 
 export default function Home() {
 
@@ -13,21 +12,23 @@ export default function Home() {
     <main>
       <Hero />
       <section>
-        <h2>Projects</h2>
-        <div className="project-list">
-          {postMetadata.map((post) => {
-            return (
-              <ProjectCard 
-                key={post.slug} 
-                post={post}
+        <div className="wrapper">
+          <h2>Projects</h2>
+          <div className="project-list">
+            {postMetadata.map((post) => {
+              return (
+                <ProjectCard
+                  key={post.slug}
+                  post={post}
                 // slug={post.slug} 
                 // date={post.date} 
                 // role={post.role} 
                 // task={post.task} 
                 // solution={post.solution} 
-              />
-            )
-          })}
+                />
+              )
+            })}
+          </div>
         </div>
       </section>
     </main>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,17 +1,25 @@
-// import Image from "next/image";
+import Image from "next/image";
+import styles from "./Hero.module.scss";
+//use styles.classname if using the css module
+
 export default function Hero() {
   return (
-    <section>
-    {/* <Image src='/images/logo.png' alt='Logo' width={400} height={400}/> */}
-      <h1>UX & Product Design</h1>
-      <ul>
-        <li>Design Systems</li>
-        <li>UX Research</li>
-        <li>Branding</li>
-        <li>NextJS & CSS</li>
-        <li>Illustration</li>
-        <li>Typograpghy</li>
-      </ul>
+    <section className={styles.hero}>
+      <div className={'wrapper ' + styles.wrapper}>
+        <Image src='/images/logo.png' alt='Logo' width={400} height={400} />
+        <div className={styles.text}>
+          <h1>UX & Product Design</h1>
+          <ul>
+            <li>Design Systems</li>
+            <li>UX Research</li>
+            <li>Branding</li>
+            <li>NextJS & CSS</li>
+            <li>Illustration</li>
+            <li>Typograpghy</li>
+          </ul>
+        </div>
+      </div>
+
     </section>
   );
 }
