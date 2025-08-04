8b4341734bf8b99b471a847c57def948254525bc
Revenciel <clarkybox@gmail.com>
2025-08-04 11:30:47 -0400

    Add CTA hover effect

--- components/CTA/CTA.tsx
+++ components/CTA/CTA.tsx
@@ -2,9 +2,14 @@ import Link from 'next/link';
 
 export default function CTA(props: { label: string, href: string }) {
     return (
-        <Link className='cta' href={props.href}>
-            {props.label}
-            <img src='/images/arrow.svg' className='icon' aria-hidden='true'></img>
-        </Link>
+        <div className='cta'>
+            <Link href={props.href}>
+                <span className="label">
+                    {props.label}
+                    <img src="/images/arrow.svg" className="icon" aria-hidden="true" />
+                </span>
+            </Link>
+
+        </div>
     );
 }
