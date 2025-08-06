e4b91e5420efea52798d10afa4e63dfa3aed2dab
Revenciel <clarkybox@gmail.com>
2025-08-06 18:38:28 -0400

    styling footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
 export default function Footer() {
+
+  new Date().getFullYear()
+
+
   return (
-    <footer>
-      Placeholder for footer component
+    <footer className='band'>
+      <div className='wrapper'>
+        <Image src='/images/logo-80-mask.png' alt='Clarkybox logo' width='80' height='80'></Image>
+        <div className='text'>
+          <span>This portfolio uses React & NextJS. Check out the code on <Link href='https://github.com/Revenciel/portfolio-2025/' target='_blank'>GitHub</Link></span>
+          <span>
+            ©{`${new Date().getFullYear()} Laura "Clarky" Clarkson`}
+          </span>
+        </div>
+      </div>
     </footer>
   )
 }
