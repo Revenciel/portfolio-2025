044b9ed824446b5547e0aa388b08d2e098526ac0
Revenciel <clarkybox@gmail.com>
2025-08-08 09:25:24 -0400

    external link icon for github

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -78,7 +78,10 @@ export default function Nav() {
         </li>
         <li><Link href="/#projects">Projects</Link></li>
         <li><Link href="/#illustration">Illustration</Link></li>
-        <li><Link href="https://github.com/Revenciel/portfolio-2025" target='_blank'>GitHub</Link></li>
+        <li><Link href="https://github.com/Revenciel/portfolio-2025" target='_blank'>
+          GitHub
+          <img src='/images/icon-external-link.svg' className="icon" aria-hidden="true" alt=''/>
+        </Link></li>
         <li><Link href="/#contact">Contact</Link></li>
       </ul>
     </nav>
